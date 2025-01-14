2571e60a7a75cdd2c8fc4a3921678d3a519f1c82
MathisMchd <michenaudmathis@gmail.com>
2025-01-14 13:01:58 +0100

    sources images et implémentation avec la config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./**/*.html'],
   theme: {
-    extend: {},
+    colors: {
+      'primaryColor': '#4C9EEB',
+      'text-primary-color': '#141619',
+      'text-secondary-color': '#687684',
+      'accent-color': '#4C9EEB',
+      'line-color': '#CED5DC',
+      'red': '#DC0000'
+    },
+    fontSize: {
+      'text-small': '12px',
+      'text-medium': '14px',
+      'text-large': '16px',
+    },
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    extend: {
+      fontFamily: {
+        'sf-pro': ['sf-pro-text', 'sans-serif'],
+      },
+    }
   },
   plugins: [],
 }
