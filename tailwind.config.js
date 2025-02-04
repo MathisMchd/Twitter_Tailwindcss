016f224118e52f5dcf0fc37a0962ea99f1b0e08d
MathisMchd <michenaudmathis@gmail.com>
2025-02-04 17:26:50 +0100

    design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ['./**/*.html'],
   theme: {
     colors: {
+      'bg-color-white': '#FFFFFF',
       'primaryColor': '#4C9EEB',
       'text-primary-color': '#141619',
       'text-secondary-color': '#687684',
@@ -19,11 +20,39 @@ module.exports = {
       center: true,
       padding: '2rem'
     },
+
     extend: {
       fontFamily: {
         'sf-pro': ['sf-pro-text', 'sans-serif'],
       },
+      width: {
+        'icon-md': '15px',
+        'icon-sm': '12px',
+        'line-vertical': '2px'
+      },
+      maxWidth: {
+        'image': '55px',
+        'image-sm': '37px',
+      },
+      height: {
+        'icon-md': '15px',
+        'icon-sm': '12px'
+      },
+      maxHeight: {
+        'image': '55px',
+        'image-sm': '37px',
+      },
+      flex: {
+        '15': '15%',
+        '85': '85%'
+      },
+      spacing: {
+        '2px': '2px'
+      }
     }
+  },
+  corePlugins: {
+    preflight: true
   },
   plugins: [],
 }
